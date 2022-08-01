420167749050ceba4ce2b13ecd32d398768b333d
mhkeller <code@mhkeller.com>
2022-08-01 15:41:42 -0400

    add exports

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+export { default as checkIsTrue } from './lib/checkIsTrue.js';
 export { default as commas } from './lib/commas.js';
-export { default as checkExists } from './lib/checkExists.js';
+export { default as readDataset } from './lib/readDataset.js';
+export { default as requireArg } from './lib/requireArg.js';
 export { default as sleep } from './lib/sleep.js';
-export { default as writeDataSync } from './lib/writeDataSync.js';
+export { default as takeEvery } from './lib/takeEvery.js';
+export { default as toTitleCase } from './lib/toTitleCase.js';
+export { default as writeSync } from './lib/writeSync.js';
