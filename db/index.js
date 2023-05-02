d3c0ca56181957e9863f1a0bc65adc37d6399e3d
mhkeller <code@mhkeller.com>
2023-05-02 15:47:28 -0400

    more reorganization

---
+++ db/index.js
@@ -0,0 +1,2 @@
+export { default as connectPg } from './connectPg.js';
+export { default as setTableUpload } from './setTableUpload.js';
---
+++ scrape/index.js
@@ -0,0 +1,3 @@
+export { default as launchBrowser } from './launchBrowser.js';
+export { default as screenshotPage } from './screenshotPage.js';
+export { default as wait } from './wait.js';
