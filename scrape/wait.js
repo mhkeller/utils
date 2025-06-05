4554d675bf97033115d47b79ebc0f2b788fa72b3
mhkeller <code@mhkeller.com>
2025-06-05 19:24:42 -0400

    add more types

--- scrape/wait.js
+++ scrape/wait.js
@@ -1,16 +1,19 @@
 import notify from '@mhkeller/notify';
 
 /**
- * Wait for a specified amount of time
- * @param {Object} page The puppeteer or playwright page object
+ * Create a function to wait for a specified amount of time on a page
+ * @param {import('playwright').Page} page - The puppeteer or playwright page object
  * @returns {Function} A function that waits for a specified amount of time
  */
 export default function wait(page) {
 	/**
+	 * Wait for the specified time
 	 * @param {number} ms - The number of milliseconds to wait
+	 * @param {string} [msg=''] - Optional message to display while waiting
+	 * @returns {Promise<void>} - Promise that resolves when waiting is complete
 	 */
-	return async function waitFor (ms, msg = '') {
+	return async function waitFor(ms, msg = '') {
 		notify({ m: 'Waiting...', v: `${ms / 1_000}s ${msg}`, d: ['gray', 'italic'] });
 		return page.waitForTimeout(ms);
-	}
+	};
 }
