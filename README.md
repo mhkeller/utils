3974ade9cdf2f1d05e796ce7156376a971ccbd2a
mhkeller <code@mhkeller.com>
2025-07-13 23:28:06 -0400

    add test CI

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 Utils
 ===
 
+[![Tests](https://github.com/mhkeller/utils/workflows/Tests/badge.svg)](https://github.com/mhkeller/utils/actions/workflows/test.yml)
+[![Code Quality](https://github.com/mhkeller/utils/workflows/Code%20Quality/badge.svg)](https://github.com/mhkeller/utils/actions/workflows/quality.yml)
+
 > A collection of utility functions
 
 ## Installing
--- vitest.config.js
+++ vitest.config.js
@@ -6,8 +6,23 @@ export default defineConfig({
 		globals: true,
 		coverage: {
 			provider: 'v8',
-			reporter: ['text', 'json', 'html'],
-			exclude: ['node_modules/', 'test/', '**/*.test.js', '**/*.spec.js']
+			reporter: ['text', 'json', 'html', 'lcov'],
+			exclude: [
+				'node_modules/',
+				'test/',
+				'**/*.test.js',
+				'**/*.spec.js',
+				'.github/',
+				'coverage/',
+				'vitest.config.js'
+			],
+			include: ['lib/**/*.js', 'db/**/*.js', 'scrape/**/*.js', 'index.js'],
+			thresholds: {
+				lines: 80,
+				functions: 80,
+				branches: 70,
+				statements: 80
+			}
 		}
 	}
 });
